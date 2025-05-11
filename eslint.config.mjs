f868bb0ed696ca9d8b5151988245c5a8e0584df1
NotHarshhaa <reddyharshhaa12@gmail.com>
2025-05-11 12:18:40 +0530

    blog: chore(eslint): update ESLint configuration to include Prettier rules
    - Modified ESLint configuration to export an array, adding Prettier rules.
    - Disabled Prettier formatting rule for better integration with existing code style.
    
    Signed-off-by: NotHarshhaa <reddyharshhaa12@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,15 @@
 import tszhong0411 from '@tszhong0411/eslint-config'
 
-export default tszhong0411({
-  project: './tsconfig.json',
-  tsconfigRootDir: import.meta.dirname,
-  react: true,
-  next: true
-})
+export default [
+  tszhong0411({
+    project: './tsconfig.json',
+    tsconfigRootDir: import.meta.dirname,
+    react: true,
+    next: true,
+  }),
+  {
+    rules: {
+      'prettier/prettier': 'off',
+    },
+  },
+]
